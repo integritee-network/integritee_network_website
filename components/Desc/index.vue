5703a307479f15c007eb1af60f601af465632e47
Laust <artsd@mail.ru>
2023-06-03 06:11:03 +0400

    add usecases page

--- components/Desc/index.vue
+++ components/Desc/index.vue
@@ -12,7 +12,7 @@
           <a href="#" class="btn btn_gradient desc__button">
             <slot name='button' />
           </a>
-            <slot name='docs' />
+          <slot name='docs' />
         </div>
       </div>
     </div>
@@ -21,32 +21,44 @@
 
 <style lang="scss" scoped>
 :slotted(span) {
-    display: inline-block;
-    background: linear-gradient(90deg, #30DAFF -20.25%, #B000FD 50%);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
+  display: inline-block;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 }
+
+:slotted(span:first-child) {
+  background: linear-gradient(90deg, #30DAFF -20.25%, #B000FD 50%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
 :slotted(.docs__button) {
   border: 2px solid rgba(255, 255, 255, 0.2);
   background: transparent;
   backdrop-filter: blur(30px);
 }
+
 .desc {
   position: relative;
   padding-top: 90px;
+
   @include slg {
     padding-top: 108px;
   }
+
   @include sm {
     padding-top: 68px;
   }
+
   &__title {
     line-height: 110%;
   }
+
   &__row {
     display: flex;
     align-items: center;
     gap: 18px;
+
     @include sm {
       gap: 12px;
     }
--- pages/useCases.vue
+++ pages/useCases.vue
@@ -1,8 +1,10 @@
 <template>
+  <Hero />
   <List />
   <Socials />
 </template>
 <script setup>
+import Hero from '@/components/UseCases/Hero.vue'
 import List from '@/components/UseCases/List.vue'
 import Socials from '@/components/Socials/Section.vue'
 </script>
