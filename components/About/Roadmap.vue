224216764c573425bc4bb990a93c1f8714e3ede2
Laust <artsd@mail.ru>
2023-06-04 06:50:09 +0400

    add roadmap block

--- components/About/Roadmap.vue
+++ components/About/Roadmap.vue
@@ -1,28 +1,300 @@
 <template>
-  <section id="roadmap">
+  <section id="roadmap" ref="root">
     <div class="roadmap block">
       <div class="container">
         <h2 class="title title_h2 margin_medium">Project roadmap</h2>
         <p class="roadmap__description margin_large paragraph paragraph_medium">Here’s what we’re building and just as
           importantly, what we’ve
           already built</p>
-        <div class="row roadmap__row">
-          <div class="roadmap__list-years">
-            <button v-for="year in years" class="roadmap__item-year" :class="{ active: year.active }" type=" button"
-              :key="year.year">
-              {{ year.year }}
-            </button>
+        <div class="row roadmap__row" ref="parent">
+          <div class="roadmap__list-years-wrapper">
+            <div class="roadmap__list-years" :class="{ active: fixedNav, end: endNav }" ref="nav">
+              <button v-for="(year, idx) in years" class="roadmap__item-year" :class="{ active: year.active }"
+                type="button" @click="onYearClick(idx)" :key="year.year">
+                {{ year.year }}
+              </button>
+            </div>
+          </div>
+          <div class="roadmap__list-text">
+            <div v-for="(year, idx) in roadmap" ref="items" class="roadmap__item-text" :class="{ active: year.active }"
+              :key="idx">
+              <div v-for="(quartal, index) in year.info" class="roadmap__item-text-quartal"
+                :class="{ full: quartal.type === 'long' }" :key="index">
+                <div class="roadmap__item-text-name">{{ quartal.name }}</div>
+                <div class="roadmap__item-text-points-wrapper">
+                  <span v-for="(point, pIndex) in quartal.content"
+                    class="roadmap__item-text-point paragraph paragraph_medium" :key="pIndex">
+                    {{ point }}
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </section>
 </template>
-<script setup>
+<script setup lang="ts">
 import useRoadmap from '@/hooks/useRoadmap'
+import { useWindowScroll } from '@vueuse/core'
+import { ref, watch } from 'vue'
+import { useWindowSize } from '@vueuse/core'
+import { clone } from 'lodash'
+import { breakpoints } from '@/configs/app.config'
 
-const { getYears } = useRoadmap()
+const { getYears, getInfoList, setActive } = useRoadmap()
+
+const { height, width } = useWindowSize()
+const { y } = useWindowScroll()
+
+const items = ref<HTMLElement[]>([])
+const parent = ref<HTMLElement | null>(null)
+const nav = ref<HTMLElement | null>(null)
+const root = ref<HTMLElement | null>(null)
+
+const fixedNav = ref(false)
+const endNav = ref(false)
 
 const years = getYears()
+const roadmap = getInfoList()
+
+const onYearClick = (idx: number) => {
+  if (!root.value) return
+  if (width.value > breakpoints.sm) {
+    window.scrollTo({ top: items.value[idx].offsetTop + root.value.offsetTop - height.value / 2 + items.value[idx].offsetHeight / 2, behavior: "smooth" })
+  } else {
+    window.scrollTo({ top: items.value[idx].offsetTop + root.value.offsetTop - 84, behavior: "smooth" })
+  }
+}
+
+watch(y, (value) => {
+  if (!parent.value || !nav.value || !root.value) return
+
+  const middleHeight = height.value / 2
+
+  if (width.value > breakpoints.sm) {
+    const elementInMiddle = parent.value?.getBoundingClientRect().y + nav.value.offsetHeight / 2
+    const elementInEnd = parent.value?.getBoundingClientRect().y + parent.value.offsetHeight - height.value / 2 - nav.value.offsetHeight / 2
+
+    fixedNav.value = middleHeight >= elementInMiddle && elementInEnd > 0
+    endNav.value = elementInEnd <= 0
+  } else {
+    const elementInEnd = parent.value?.getBoundingClientRect().y + parent.value.offsetHeight
+
+    fixedNav.value = parent.value?.getBoundingClientRect().y <= 84
+    endNav.value = elementInEnd <= 84
+  }
+
+  const clonedItems = clone(items.value)
+  for (const [idx, item] of clonedItems.reverse().entries()) {
+    if (item.getBoundingClientRect().y - height.value / 2 <= 0) {
+      setActive(years[years.length - 1 - idx].year)
+      break
+    }
+  }
+})
+
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.roadmap {
+  &__row {
+    gap: 20px;
+
+    @include sm {
+      position: relative;
+      display: block;
+    }
+  }
+
+  &__description {
+    @include sm {
+      margin-bottom: 32px;
+    }
+  }
+
+  &__list-years {
+    @include sm {
+      margin-bottom: 10px;
+      display: flex;
+      gap: 8px;
+      align-items: flex-start;
+    }
+
+    &.active {
+      position: fixed;
+      top: 50%;
+      transform: translateY(-50%);
+
+      @include sm {
+        top: 76px;
+        left: 0;
+        background: rgba(81, 81, 81, 0.146);
+        backdrop-filter: blur(38.335px);
+        transform: none;
+        padding: 8px 0 8px 21px;
+        z-index: 5;
+      }
+    }
+
+    &.end {
+      position: absolute;
+      bottom: 0;
+    }
+  }
+
+  &__list-years-wrapper {
+    position: relative;
+
+    @include sm {
+      position: static;
+    }
+  }
+
+  &__list-years-wrapper,
+  &__list-years {
+    width: 137px;
+
+    @include sm {
+      width: 100%;
+      height: 56px;
+    }
+  }
+
+  &__list-years-wrapper {
+    flex-shrink: 0;
+  }
+
+  &__item-year {
+    width: 100%;
+    font-size: 1.5em;
+    margin-bottom: 20px;
+    border-radius: 24px;
+    background: rgba(81, 81, 81, 0.246);
+    backdrop-filter: blur(38px);
+    padding: 24px 0;
+    border: 0;
+    color: #fff;
+    font-weight: 500;
+    transition: .3s ease;
+    cursor: pointer;
+
+    @include sm {
+      font-size: .875em;
+      padding: 12px 25px;
+      flex-shrink: 0;
+      width: auto;
+      margin-bottom: 0;
+      border-radius: 8px;
+    }
+
+    &.active {
+      background: rgba(133, 133, 133, 0.25);
+    }
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  &__item-text-name {
+    font-size: 2em;
+    margin-bottom: 32px;
+
+    @include sm {
+      font-size: 1.125em;
+      margin-bottom: 18px;
+    }
+  }
+
+  &__item-text-point {
+    position: relative;
+    padding-left: 25px;
+    display: block;
+
+    @include sm {
+      padding-left: 20px;
+      margin-bottom: 15px;
+    }
+
+    &:before {
+      position: absolute;
+      content: '';
+      left: 9px;
+      top: 11px;
+      width: 5px;
+      height: 5px;
+      background: #fff;
+      border-radius: 50%;
+
+      @include sm {
+        width: 3px;
+        height: 3px;
+        top: 10px;
+      }
+    }
+  }
+
+  &__item-text {
+    display: grid;
+    grid-template-columns: repeat(2, 2fr);
+    grid-column-gap: 20px;
+    grid-row-gap: 20px;
+    margin-bottom: 20px;
+    opacity: 0.5;
+
+    @include sm {
+      display: block;
+      margin-bottom: 10px;
+    }
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+
+    &.active {
+      opacity: 1;
+    }
+  }
+
+  &__item-text-quartal {
+    padding: 60px;
+    background: rgba(120, 120, 120, 0.25);
+    backdrop-filter: blur(12px);
+    border-radius: 24px;
+    transition: .3s ease;
+
+    @include sm {
+      border-radius: 14px;
+      padding: 40px;
+      margin-bottom: 10px;
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+
+    &.full {
+      grid-area: 1 / 1 / 2 / 3;
+
+      .roadmap__item-text-points-wrapper {
+        display: flex;
+        flex-wrap: wrap;
+
+        @include sm {
+          display: block;
+        }
+
+        .roadmap__item-text-point {
+          width: 50%;
+
+          @include sm {
+            width: 100%;
+          }
+        }
+      }
+    }
+  }
+
+}
+</style>
--- hooks/useRoadmap.ts
+++ hooks/useRoadmap.ts
@@ -1,10 +1,28 @@
-import { ref, Ref, reactive } from 'vue'
+import { reactive } from 'vue'
+import { objectEntries } from '@vueuse/core'
+import map from 'lodash/map'
+
+interface Year {
+  year: string
+  active: boolean
+}
+
+interface Roadmap {
+  [key: string]: {
+    info: {
+      name: string
+      content: string[]
+      type?: 'long'
+    }[]
+    active: boolean
+  }
+}
 
 export default function useRoadmap() {
-  const years = reactive([
+  const years = reactive<Year[]>([
     {
       year: '2023',
-      active: false,
+      active: true,
     },
     {
       year: '2022',
@@ -28,7 +46,7 @@ export default function useRoadmap() {
     },
   ])
 
-  const roadmap = {
+  const roadmap = reactive<Roadmap>({
     '2023': {
       info: [
         {
@@ -39,9 +57,10 @@ export default function useRoadmap() {
             'Bridge to other Ecosystems',
             'Deploy first enterprise PoCs',
           ],
+          type: 'long',
         },
       ],
-      type: 'long',
+      active: true,
     },
     '2022': {
       info: [
@@ -94,12 +113,125 @@ export default function useRoadmap() {
           ],
         },
       ],
-      type: 'long',
+      active: false,
     },
+    '2021': {
+      info: [
+        {
+          name: 'Q1',
+          content: [
+            'Launch Rococo Testnet Parachain',
+            'Conceptualize and Preparation of Spin-off',
+          ],
+        },
+        {
+          name: 'Q2',
+          content: [
+            'Spin-off from SCS and birth of Integritee AG',
+            'Seed Round Close',
+          ],
+        },
+        {
+          name: 'Q3',
+          content: [
+            'New Brand and Website relaunch',
+            'Move from SubstraTEE to Integritee',
+            'Release Tokenomics',
+            'Participation in public slot auctions on Kusama',
+            'Mainnet & Token Launch',
+            'Start of Community Growth',
+          ],
+        },
+        {
+          name: 'Q4',
+          content: [
+            'First deployment and practical use case hosted on the Integritee mainnet will be launched: the US dollar exchange rate oracle.',
+            'List TEER on major exchange',
+            'Start of Integritee Ambassador Program',
+          ],
+        },
+      ],
+      active: false,
+    },
+    '2020': {
+      info: [
+        {
+          name: 'Q1',
+          content: ['3rd Web3 Grant for: SubstraTEE extension pack'],
+        },
+        {
+          name: 'Q2-Q3',
+          content: [
+            'Development Prototype Platform based on SubstraTEE.',
+            'First deployed and running use case with Encointer on the Platform',
+          ],
+        },
+        {
+          name: 'Q4',
+          content: ['Polkadot Treasury Grant “SubstraTEE scalability”'],
+        },
+      ],
+      active: false,
+    },
+    '2019': {
+      info: [
+        {
+          name: 'Q1',
+          content: [
+            '1st Web3 Grant for: Substrate Transaction Privacy using Intel SGX',
+          ],
+        },
+        {
+          name: 'Q3',
+          content: ['2nd Web3 Grant for: Substrate Rust API client'],
+        },
+        {
+          name: 'Q4',
+          content: [
+            'First SubstraTEE Meetup to demonstrate private token transactions.',
+          ],
+        },
+      ],
+      active: false,
+    },
+    '2018': {
+      info: [
+        {
+          name: 'Q4',
+          content: [
+            'Release of the Encointer Whitepaper – “Local Community Cryptocurrencies with Universal Basic Income” and start of the SubstraTEE journey',
+          ],
+        },
+      ],
+      active: false,
+    },
+  })
+
+  function setActive(year: string) {
+    map(roadmap, (item) => {
+      item.active = false
+    })
+    roadmap[year].active = true
+
+    const activeYear = years.find((item) => item.year === year)
+    if (activeYear) {
+      map(years, (item) => {
+        item.active = false
+      })
+      activeYear.active = true
+    }
   }
 
   function getYears() {
     return years
   }
-  return { getYears }
+
+  function getInfoList() {
+    const sort = objectEntries(roadmap)
+      .map((item) => item[1])
+      .reverse()
+    return sort
+  }
+
+  return { getYears, getInfoList, setActive }
 }
